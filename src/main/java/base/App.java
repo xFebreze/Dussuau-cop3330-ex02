f4104ffbd30f2c5a4465e0b4d6d588c261d2984e
Alek Dussuau <ajdussuau@gmail.com>
2021-06-06 11:28:31 -0400

    Initial Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,7 @@
 package base;
+
+import java.util.Scanner;
+
 /*
 UCF COP3330 Summer 2021 Assignment 1 Solution
 Copyright 2021 Alek Dussuau
@@ -25,6 +28,17 @@ instead.
 */
 public class App {
     public static void main(String[] args){
-        
+        //variables
+        Scanner input = new Scanner(System.in);
+
+        //input
+        System.out.print("What is the input string? ");
+        String inputString = input.nextLine();
+
+        //calc
+        int length = inputString.length();
+
+        //output
+        System.out.println(inputString + " has " + length + " characters.");
     }
 }
